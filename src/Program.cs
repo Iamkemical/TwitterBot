454b995668a40d8a336e6febc34102bd7eee0b9d
Gabriel Isaac <iamkemical1@gmail.com>
2021-02-15 12:57:48 +0100

    Created Twitter Bot Project

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TwitterBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
